0156f08481509ed55e85fde1a855f4378af4e90c
Daniil <danilayukin@gmail.com>
2025-01-22 16:35:19 +0300

    /

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,22 @@
-from .models import Product
 from django.views.generic import DetailView, ListView, TemplateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .forms import ProductForm
+from .models import Product
+
+
+class ProductCreateView(CreateView):
+    model = Product
+    form_class = ProductForm
+    template_name = 'catalog/product_form.html'
+    success_url = reverse_lazy('catalog:product_list')
+
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    form_class = ProductForm
+    template_name = 'catalog/product_form.html'
+    success_url = reverse_lazy('catalog:product_list')
 
 
 class ProductDetailView(DetailView):
@@ -12,6 +29,13 @@ class ProductListView(ListView):
     model = Product
     template_name = 'catalog/product_list.html'
     context_object_name = 'products'
+
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    template_name = 'catalog/product_confirm_delete.html'
+    success_url = reverse_lazy('catalog:product_list')
+
 
 
 class ProductTemplateView(TemplateView):
